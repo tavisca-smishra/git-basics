760527508091dfd023055b8c8519ec3330b8458d
tavisca-smishra <smishra@tavisca.in>
2019-06-17 13:31:56 +0530

    Hello yses Greeter class

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,4 +1,9 @@
 # Unwanted but staged commit
 # Author : Saswath Mishra (smishra@tavisca.in)
+require 'greeter'
 name = ARGV.first || "World"
 puts "Hello, #{name}!"
+
+greeter = Greeter.new(name)
+puts greeter.greet
+
